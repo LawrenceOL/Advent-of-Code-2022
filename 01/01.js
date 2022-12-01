3ee6ab02d264460809914d956f21998cc2e1778e
Lawrence <loreganlloyd@gmail.com>
2022-12-01 14:37:09 -0500

    Solve day one part two

--- 01/01.js
+++ 01/01.js
@@ -1,3 +1,5 @@
+// Part Two
+
 const fs = require("fs");
 
 fs.readFile("01-input.txt", (err, data) => {
@@ -7,20 +9,51 @@ fs.readFile("01-input.txt", (err, data) => {
 
   let input_arr = inputStr.split("\n");
 
-  let highest_calories = 0;
+  let calories_per_elf = [];
 
   let new_calories = 0;
 
   for (let i = 0; i < input_arr.length; i++) {
     if (input_arr[i] !== "") {
       new_calories = new_calories + Number(input_arr[i]);
-      if (new_calories >= highest_calories) {
-        highest_calories = new_calories;
-      }
     } else {
+      calories_per_elf.push(new_calories);
       new_calories = 0;
     }
   }
 
-  console.log(highest_calories);
+  calories_per_elf.sort(function (a, b) {
+    return b - a;
+  });
+
+  console.log(calories_per_elf.slice(0, 3).reduce((a, b) => a + b));
 });
+
+// Part One
+
+// const fs = require("fs");
+
+// fs.readFile("01-input.txt", (err, data) => {
+//   if (err) throw err;
+
+//   inputStr = data.toString();
+
+//   let input_arr = inputStr.split("\n");
+
+//   let highest_calories = 0;
+
+//   let new_calories = 0;
+
+//   for (let i = 0; i < input_arr.length; i++) {
+//     if (input_arr[i] !== "") {
+//       new_calories = new_calories + Number(input_arr[i]);
+//       if (new_calories >= highest_calories) {
+//         highest_calories = new_calories;
+//       }
+//     } else {
+//       new_calories = 0;
+//     }
+//   }
+
+//   console.log(highest_calories);
+// });

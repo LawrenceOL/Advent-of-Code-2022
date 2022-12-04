71c89791dab7631cf8e752924dc6c6ce44b02287
Lawrence O'Regan-Lloyd <loreganlloyd@gmail.com>
2022-12-04 00:05:20 -0500

    solve part one

--- 03/03.js
+++ 03/03.js
@@ -7,6 +7,8 @@ fs.readFile("03-input.txt", (err, data) => {
 
   let input_arr = inputStr.split("\n");
 
+  let dupsArray = [];
+
   let prioritesSum = 0;
 
   for (let i = 0; i < input_arr.length; i++) {
@@ -14,7 +16,7 @@ fs.readFile("03-input.txt", (err, data) => {
     for (let j = 0; j < input_arr[i].length / 2; j++) {
       lineSet.add(input_arr[i][j]);
     }
-    for (let j = input_arr[i].length / 2 + 1; j < input_arr[i].length; j++) {
+    for (let j = input_arr[i].length / 2; j < input_arr[i].length; j++) {
       if (lineSet.has(input_arr[i][j])) {
         if (input_arr[i][j] === input_arr[i][j].toUpperCase()) {
           prioritesSum = prioritesSum + input_arr[i][j].charCodeAt(0) - 38;
@@ -26,5 +28,6 @@ fs.readFile("03-input.txt", (err, data) => {
       }
     }
   }
+
   console.log(prioritesSum);
 });
